cd9e7c59b55d5bbb02b4608a18fa083550770f64
miminmoons <jasmine.saphira@mail.utoronto.ca>
2024-10-28 17:50:55 -0400

    Fixed CheckStyle

--- src/main/java/app/MainWithInMemory.java
+++ src/main/java/app/MainWithInMemory.java
@@ -1,8 +1,6 @@
 package app;
 
-import data_access.DBUserDataAccessObject;
 import data_access.InMemoryUserDataAccessObject;
-import entity.CommonUserFactory;
 import interface_adapter.ViewManagerModel;
 import interface_adapter.logged_in.LoggedInViewModel;
 import interface_adapter.login.LoginViewModel;
@@ -12,8 +10,8 @@ import view.LoginView;
 import view.SignupView;
 import view.ViewManager;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  * The version of Main with an external database used to persist user data.
